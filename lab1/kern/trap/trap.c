1ce166d8ed5a3c87ab8b2d9f9879ad950c5f9631
open-sun <1695730274@qq.com>
2024-09-24 22:22:53 +0800

    lab1

--- lab1/kern/trap/trap.c
+++ lab1/kern/trap/trap.c
@@ -101,17 +101,30 @@ void interrupt_handler(struct trapframe *tf) {
             cprintf("User software interrupt\n");
             break;
         case IRQ_S_TIMER:
-            // "All bits besides SSIP and USIP in the sip register are
+             // "All bits besides SSIP and USIP in the sip register are
             // read-only." -- privileged spec1.9.1, 4.1.4, p59
             // In fact, Call sbi_set_timer will clear STIP, or you can clear it
             // directly.
             // cprintf("Supervisor timer interrupt\n");
-             /* LAB1 EXERCISE2   YOUR CODE :  */
+             /* LAB1 EXERCISE2   2212422 :  */
+            
             /*(1)设置下次时钟中断- clock_set_next_event()
              *(2)计数器（ticks）加一
              *(3)当计数器加到100的时候，我们会输出一个`100ticks`表示我们触发了100次时钟中断，同时打印次数（num）加一
             * (4)判断打印次数，当打印次数为10时，调用<sbi.h>中的关机函数关机
             */
+           clock_set_next_event();
+           ticks+=1;
+           if(num==10)
+           {
+            print_ticks();
+            sbi_shutdown();
+           }
+           else if(ticks%TICK_NUM==0)
+           {
+            print_ticks();
+            num+=1;
+           }
             break;
         case IRQ_H_TIMER:
             cprintf("Hypervisor software interrupt\n");
