da9e16378b0338a432dd07db27012ff2abd7e4a3
open-sun <1695730274@qq.com>
2024-09-25 01:07:08 +0800

    lab1 chanllenge

--- lab1/kern/trap/trap.c
+++ lab1/kern/trap/trap.c
@@ -157,19 +157,25 @@ void exception_handler(struct trapframe *tf) {
             break;
         case CAUSE_ILLEGAL_INSTRUCTION:
              // 非法指令异常处理
-             /* LAB1 CHALLENGE3   YOUR CODE :  */
+             /* LAB1 CHALLENGE3   YOUR CODE : 2212422*/
             /*(1)输出指令异常类型（ Illegal instruction）
              *(2)输出异常指令地址
              *(3)更新 tf->epc寄存器
             */
+           cprintf("Exception type:Illegal instruction\n");
+           cprintf("Illegal instruction caught at0x%08x\n", tf->epc);
+
             break;
         case CAUSE_BREAKPOINT:
             //断点异常处理
-            /* LAB1 CHALLLENGE3   YOUR CODE :  */
+            /* LAB1 CHALLLENGE3   YOUR CODE : 2212422 */
             /*(1)输出指令异常类型（ breakpoint）
              *(2)输出异常指令地址
              *(3)更新 tf->epc寄存器
             */
+           cprintf("Exception type: breakpoint\n");
+           cprintf(" ebreak caught at0x%08x\n", tf->epc);
+
             break;
         case CAUSE_MISALIGNED_LOAD:
             break;
