ba3d8d1ef463a1fe9ac74a491be8f6f601355eb2
Liujuntong0204 <2984343291@qq.com>
2024-09-27 20:08:31 +0800

    lab1

--- lab1/kern/init/init.c
+++ lab1/kern/init/init.c
@@ -32,6 +32,10 @@ int kern_init(void) {
     clock_init();  // init clock interrupt
 
     intr_enable();  // enable irq interrupt
+
+    asm("mret");
+    asm("ebreak");
+    
     
     while (1)
         ;
