265706e1c6e3ba8cb776d19ac5f3a39cac937be6
LiuYuhan <1458753308@qq.com>
2024-11-10 19:43:49 +0800

    lab3

--- riscv64-ucore-labcodes/lab3/kern/mm/swap_clock.c
+++ riscv64-ucore-labcodes/lab3/kern/mm/swap_clock.c
@@ -25,7 +25,7 @@
  *              le2page (in memlayout.h), (in future labs: le2vma (in vmm.h), le2proc (in proc.h),etc.
  */
 
-list_entry_t pra_list_head, *curr_ptr;
+list_entry_t pra_list_headc, *curr_ptr;
 /*
  * (2) _fifo_init_mm: init pra_list_head and let  mm->sm_priv point to the addr of pra_list_head.
  *              Now, From the memory control struct mm_struct, we can access FIFO PRA
@@ -38,6 +38,10 @@ _clock_init_mm(struct mm_struct *mm)
      // 初始化当前指针curr_ptr指向pra_list_head，表示当前页面替换位置为链表头
      // 将mm的私有成员指针指向pra_list_head，用于后续的页面替换算法操作
      //cprintf(" mm->sm_priv %x in fifo_init_mm\n",mm->sm_priv);
+     list_init(&pra_list_headc);
+     curr_ptr = &pra_list_headc;
+     mm->sm_priv = &pra_list_headc;
+     cprintf(" mm->sm_priv %x in f_mm\n",mm->sm_priv);
      return 0;
 }
 /*
@@ -54,6 +58,9 @@ _clock_map_swappable(struct mm_struct *mm, uintptr_t addr, struct Page *page, in
     // link the most recent arrival page at the back of the pra_list_head qeueue.
     // 将页面page插入到页面链表pra_list_head的末尾
     // 将页面的visited标志置为1，表示该页面已被访问
+    list_entry_t *head=(list_entry_t*) mm->sm_priv;
+    list_add_before(head, entry);
+    page->visited = 1;
     return 0;
 }
 /*
@@ -64,6 +71,8 @@ static int
 _clock_swap_out_victim(struct mm_struct *mm, struct Page ** ptr_page, int in_tick)
 {
      list_entry_t *head=(list_entry_t*) mm->sm_priv;
+     cprintf("curr_ptr %p\n",curr_ptr);
+        cprintf("head %p\n",head);
          assert(head != NULL);
      assert(in_tick==0);
      /* Select the victim */
@@ -76,6 +85,29 @@ _clock_swap_out_victim(struct mm_struct *mm, struct Page ** ptr_page, int in_tic
         // 获取当前页面对应的Page结构指针
         // 如果当前页面未被访问，则将该页面从页面链表中删除，并将该页面指针赋值给ptr_page作为换出页面
         // 如果当前页面已被访问，则将visited标志置为0，表示该页面已被重新访问
+        curr_ptr = list_next(curr_ptr);
+        if(curr_ptr == head)
+        {
+            curr_ptr = list_next(head);
+            if(curr_ptr == head)
+            {
+                *ptr_page = NULL;
+                return 0;
+            }
+            
+        }
+        struct Page* page = le2page(curr_ptr, pra_page_link);
+        if(page->visited == 0)
+        {
+            *ptr_page = page;
+            cprintf("curr_ptr %p\n",curr_ptr);
+            list_del(curr_ptr);
+            break;
+        }
+        else
+        {
+            page->visited = 0;
+        }
     }
     return 0;
 }
