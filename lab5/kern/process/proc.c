dff794d9b3a102b90275b6132ff91b08a25ee49e
Liujuntong0204 <2984343291@qq.com>
2024-12-13 18:36:34 +0800

    lab5

--- lab5/kern/process/proc.c
+++ lab5/kern/process/proc.c
@@ -103,6 +103,18 @@ alloc_proc(void) {
      *       uint32_t flags;                             // Process flag
      *       char name[PROC_NAME_LEN + 1];               // Process name
      */
+    proc->state = PROC_UNINIT;                          
+    proc->pid = -1;                                      
+    proc->runs = 0;                                      
+    proc->kstack = 0;                                    
+    proc->need_resched = 0;                              
+    proc->parent = NULL;                                 
+    proc->mm = NULL;                                     
+    memset(&(proc->context), 0, sizeof(struct context)); 
+    proc->tf = NULL;                                     
+    proc->cr3 = boot_cr3;                                
+    proc->flags = 0;                                     
+    memset(proc->name, 0, PROC_NAME_LEN);
 
      //LAB5 YOUR CODE : (update LAB4 steps)
      /*
@@ -110,6 +122,10 @@ alloc_proc(void) {
      *       uint32_t wait_state;                        // waiting state
      *       struct proc_struct *cptr, *yptr, *optr;     // relations between processes
      */
+    proc->wait_state = 0;                                
+    proc->cptr = NULL;
+    proc->optr = NULL;
+    proc->yptr = NULL; 
     }
     return proc;
 }
@@ -206,7 +222,15 @@ proc_run(struct proc_struct *proc) {
         *   lcr3():                   Modify the value of CR3 register
         *   switch_to():              Context switching between two processes
         */
-
+        bool intr_flag;
+        struct proc_struct *prev = current, *next = proc;
+        local_intr_save(intr_flag);
+        {
+            current = proc;
+            lcr3(next->cr3);
+            switch_to(&(prev->context), &(next->context));
+        }
+        local_intr_restore(intr_flag);
     }
 }
 
@@ -403,6 +427,28 @@ do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf) {
     *    update step 1: set child proc's parent to current process, make sure current process's wait_state is 0
     *    update step 5: insert proc_struct into hash_list && proc_list, set the relation links of process
     */
+   if ((proc = alloc_proc()) == NULL)
+    {
+        goto fork_out;
+    }
+    proc->parent = current;
+    assert(current->wait_state == 0);
+    if (setup_kstack(proc) != 0)
+        goto bad_fork_cleanup_proc;
+    if (copy_mm(clone_flags, proc) != 0)
+        goto bad_fork_cleanup_kstack;
+    copy_thread(proc, stack, tf);
+
+    bool intr_flag;
+    local_intr_save(intr_flag);
+    {
+        proc->pid = get_pid();
+        hash_proc(proc);
+        set_links(proc);
+    }
+    local_intr_restore(intr_flag)
+    wakeup_proc(proc);
+    ret = proc->pid;
  
 fork_out:
     return ret;
@@ -603,7 +649,10 @@ load_icode(unsigned char *binary, size_t size) {
      *          tf->status should be appropriate for user program (the value of sstatus)
      *          hint: check meaning of SPP, SPIE in SSTATUS, use them by SSTATUS_SPP, SSTATUS_SPIE(defined in risv.h)
      */
-
+    tf->gpr.sp = USTACKTOP;
+    tf->epc = elf->e_entry;
+    // tf->status = (read_csr(sstatus) & ~SSTATUS_SPP) | SSTATUS_SPIE;
+    tf->status=sstatus = (sstatus & ~SSTATUS_SPIE) | SSTATUS_SPIE;
 
     ret = 0;
 out:
